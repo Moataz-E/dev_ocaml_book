d44ba190cf5184c9287956fa3bb3b2003cbe8e41
Moataz-E <motaz_1993@hotmail.com>
2018-07-02 01:46:52 +0100

    work so far on lexical trees

--- chapter2/lexical_trees.ml
+++ chapter2/lexical_trees.ml
@@ -2,7 +2,7 @@
 Lexical trees (or tries) are used for the representation of dictionaries.
 *)
 
-(* 
+(*
 bool in a lex_node marks the end of a valid word in the dictionary
 when the bool's value is true.
 *)
@@ -15,14 +15,21 @@ type word = string;;
 
 
 (*
-Write the function exists which tests whether a word belongs to a 
+Write the function exists which tests whether a word belongs to a
 dictionary of type lex_tree.
 *)
-let exists (w: word) (ltr: lex_tree) =
-  let aux w2 ltr2 i =
+let rec exists (w: word) (ltr: lex_tree) =
+  let aux w2 i n =
     match ltr2 with
     | [] -> false
-    | Letter (c, b, lt)::t when (c = w2.(i)) -> 
-       if (String.length w2) = 1 then b
-       else exists (String.sub w2 (i+1) ((String.length w2)-1)) t
-    | Letter (c, b, lt)::t ->  
+    | Letter (c, b, lt)::t when (c = w2.[i]) ->
+       if n = 1 then b
+       else exists (String.sub w2 (i+1) (n-1)) lt
+    | Letter (c, b, lt)::t -> exists sw t
+  in aux w 0 (String.length w)
+;;
+
+let lex_node1 = Letter ('w', false, lex_tree1)
+and lex_tree1 = [lex_node1]
+
+(* to initialize, start from the bottom to the top. *)
